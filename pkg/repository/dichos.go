28c7c6d96260ff624ce2122959a7ea1578102334
Helen Dayana Plazas Paz <helen.plazas@globant.com>
2025-12-03 17:18:47 -0500

    Lista de dichos

--- pkg/repository/dichos.go
+++ pkg/repository/dichos.go
@@ -1,3 +1,27 @@
 package repository
 
-ListDichos 
+var ListaDichos []string // Esto crea un slice vacío de enteros
+
+func AgregarDicho (dicho string) {
+	ListaDichos = append(ListaDichos,dicho)
+}
+
+
+func ObtenerDicho (indice int) string{
+
+	dicho := ListaDichos[indice]
+
+	return  dicho
+}
+
+
+func ListarDichos ()string{
+
+	cantidad := len(ListaDichos)
+	var dichos string;
+
+	for i := 0; i < cantidad; i++ {
+		dichos+= ListaDichos[i]+"\n"
+	}
+	return dichos
+}
